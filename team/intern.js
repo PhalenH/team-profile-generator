4334f23fb2a844ff938691fae0a4b2d9f3ffe4f1
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-06 10:34:06 -0400

    added employee exportmodule to other team files

--- team/intern.js
+++ team/intern.js
@@ -1,6 +1,8 @@
 // WHEN I select the intern option
 // THEN I am prompted to enter the intern’s name, ID, em
 
+const Employee = require("../team/employee");
+
 class Intern {
   constructor(name, id, email, school) {
     this.name = name;
--- team/manager.js
+++ team/manager.js
@@ -1,6 +1,8 @@
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 
+const Employee = require("../team/employee");
+
 class Manager {
   constructor(name, id, email, officeNumber) {
     this.name = name;
