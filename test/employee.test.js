3567baa350028678260896934f7a091d0beebf94
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-06 10:00:41 -0400

    fixed emplyee test cases

--- test/employee.test.js
+++ test/employee.test.js
@@ -2,18 +2,18 @@ const Employee = require("../team/employee");
 
 test("can create a name string using the contructor", () => {
     const name = "phalen";
-    const testName = new Manager(name, 10, "email@email.com", 1);
+    const testName = new Employee(name, 10, "email@email.com", 1);
     expect(testName.name).toBe(name);
   });
   
   test("can create a id number using the contructor", () => {
     const id = 10;
-    const testId = new Manager("phalen", id, "email@email.com", 1);
+    const testId = new Employee("phalen", id, "email@email.com", 1);
     expect(testId.id).toBe(id);
   });
   
   test("can create a email string using the contructor", () => {
     const email = 'email@email.com';
-    const testEmail = new Manager("phalen", 10, email, 1);
+    const testEmail = new Employee("phalen", 10, email, 1);
     expect(testEmail.email).toBe(email);
   });
