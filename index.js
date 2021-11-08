f8ac95bc288bb2c06ec07172cd3d85f121a1bcb8
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-07 22:13:38 -0500

    added functions with inquirer prompts, have to finish .then portion, added empty array for team members

--- index.js
+++ index.js
@@ -5,20 +5,199 @@ const Intern = require("./team/intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-function teamMember() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter team member name",
-      name: "name",
-      validate: function (input) {
-        if (input) {
-          return true;
-        } else {
-          console.log("Please enter a team member name!");
-          return false;
-        }
+// should this be a const? or has to be let since I'm adding stuff
+let fullTeam = [];
+
+
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your team manager's name?",
+        name: "name",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your manager's name!");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+      {
+        type: "input",
+        message: "What is your team manager's ID?",
+        name: "id",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your manager's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your team manager's email?",
+        name: "email",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your manager's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your team manager's office number?",
+        name: "office",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your manager's office number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then();
 }
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your engineer's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your engineer's ID?",
+        name: "id",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your engineer's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your engineer's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your engineer's GitHub username?",
+        name: "gitHub",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your engineer's GitHub username!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then();
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your team intern's name?",
+        name: "name",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your intern's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your intern's ID?",
+        name: "id",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your intern's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your intern's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "What is your intern's school?",
+        name: "school",
+        validate: function (input) {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter your intern's school!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then();
+}
+
+function addMembers() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Select from the following options to build your team",
+          name: "addingMembers",
+          choices: ["Add an Engineer", "Add an Intern", "Done"],
+        },
+      ])
+      .then();
+  }
+
+
+// 
